7565da9479abc171660a720ab4bbed7ed587e1cf
gabuj <gabujamesmarino@gmail.com>
2025-02-27 14:44:07 +0000

    pushing_first try

--- creating_training_set/first_try.py
+++ creating_training_set/first_try.py
@@ -1,10 +1,13 @@
 from skimage import filters
 from skimage import io
+from skimage import color
 
 image_path="shockwaves_images\Blunt_body_reentry_shapes1.png"
 image=io.imread(image_path)
 #make the image grayscale to have only one channel
-image=image[:,:,0] #
+#disregard the transparency channel
+image = image[:,:,:3]
+image = color.rgb2gray(image)
 
 #apply the sobel filter
 edges=filters.sobel(image)
