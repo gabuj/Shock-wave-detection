c04164c5267191ea99b726c1e31ba6cdf484cb5c
gabuj <gabujamesmarino@gmail.com>
2025-02-27 14:32:53 +0000

    first_change

--- creating_training_set/first_try.py
+++ creating_training_set/first_try.py
@@ -1,5 +1,14 @@
 from skimage import filters
+from skimage import io
 
-
-image_path=../
+image_path="shockwaves_images\Blunt_body_reentry_shapes1.png"
 image=io.imread(image_path)
+#make the image grayscale to have only one channel
+image=image[:,:,0] #
+
+#apply the sobel filter
+edges=filters.sobel(image)
+
+#show the image
+io.imshow(edges)
+io.show()
