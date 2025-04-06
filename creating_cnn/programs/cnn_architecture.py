86d5c213dc08cf6783ad2c225812516f1de7c050
andrac55 <criscov_andra@yahoo.com>
2025-04-06 06:42:35 +0100

    model A run on archictecture and training

--- creating_cnn/programs/cnn_architecture.py
+++ creating_cnn/programs/cnn_architecture.py
@@ -15,7 +15,7 @@ class UNet(nn.Module):
         resnet = resnet18(weights=ResNet18_Weights.IMAGENET1K_V1)
         
         # Modify ResNet's first conv layer to accept grayscale images (1 channel), maybe change kernel size to 3 and padding to 1?
-        resnet.conv1 = nn.Conv2d(in_channels=1, out_channels=64, kernel_size=7, stride=1, padding=3)
+        resnet.conv1 = nn.Conv2d(in_channels=1, out_channels=64, kernel_size=7, stride=2, padding=3)
 
         self.encoder = nn.Sequential(*list(resnet.children())[:-2])  # Remove the last two layers
         
@@ -29,7 +29,9 @@ class UNet(nn.Module):
             nn.ReLU(inplace=True),
             #output layer, size should match:(torch.Size([1, 1, 334, 409])), 
             #keeping in mind: output_size = floor((input_size + 2 * padding - kernel_size) / stride) + 1
-            nn.ConvTranspose2d(64, 1, kernel_size=4, stride=2, padding=1)
+            nn.ConvTranspose2d(64, 32, kernel_size=4, stride=2, padding=1),
+            nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(32, 1, kernel_size=4, stride=2, padding=1)
         )
         
     def forward(self, x): #x is the input image
